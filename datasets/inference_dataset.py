0d4c130b17716dd1e7b3d6be6eafdb9fb72b3aca
davidetalon <talon.davide@gmail.com>
2021-05-24 17:47:03 +0200

    inference dataset

--- datasets/inference_dataset.py
+++ datasets/inference_dataset.py
@@ -6,6 +6,24 @@ from utils import data_utils
 class InferenceDataset(Dataset):
 
 	def __init__(self, root, opts, transform=None, preprocess=None):
+
+		attributes_path = Path(root) / "list_attr_celeba.txt"
+		attr = pandas.read_csv(attributes_path, delim_whitespace=True, header=1)
+		split_map = {
+			"train": 0,
+			"valid": 1,
+			"test": 2,
+			"all": None,
+			}
+		split_ = split_map[split.lower()]
+		attributes_path = Path(root) / "list_eval_partition.txt"
+		splits = pandas.read_csv(
+			attributes_path, delim_whitespace=True, header=None, index_col=0
+        )
+        mask = slice(None) if split_ is None else (splits[1] == split_)
+        self.attr = torch.as_tensor(attr[mask].values)
+		print(attr)
+		
 		self.paths = sorted(data_utils.make_dataset(root))
 		self.transform = transform
 		self.preprocess = preprocess
@@ -23,3 +41,6 @@ class InferenceDataset(Dataset):
 		if self.transform:
 			from_im = self.transform(from_im)
 		return from_im
+
+if __name__ == '__main__':
+	dataset = InferenceDataset("/home/davidetalon/Dev/learning-self/data/raw/celeba", split='train' )
